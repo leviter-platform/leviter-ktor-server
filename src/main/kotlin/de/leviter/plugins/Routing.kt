9534eb5e5c7be05e5b0583eebc1598436965f7a8
Dominic <d151l@d151l.de>
2023-07-05 21:05:30 +0200

    add testing

--- src/main/kotlin/de/leviter/plugins/Routing.kt
+++ src/main/kotlin/de/leviter/plugins/Routing.kt
@@ -1,5 +1,6 @@
 package de.leviter.plugins
 
+import io.ktor.http.*
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
@@ -8,6 +9,15 @@ fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("test/{id?}") {
+            val id = call.parameters["id"] ?: return@get call.respondText(
+                "Missing id",
+                status = HttpStatusCode.BadRequest
+            )
+
+            call.respond(id)
         }
     }
 }
