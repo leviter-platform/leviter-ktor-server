86134850f123ecf36e7ba6200668a11154d7fac9
Dominic <d151l@d151l.de>
2023-07-05 22:13:01 +0200

    add swagger

--- src/main/kotlin/de/leviter/Application.kt
+++ src/main/kotlin/de/leviter/Application.kt
@@ -6,6 +6,8 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.plugins.swagger.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -15,7 +17,14 @@ fun main() {
 }
 
 fun Application.module() {
+    install(CORS) {
+        anyHost()
+        allowHeader(HttpHeaders.ContentType)
+    }
+
     routing {
+        swaggerUI(path = "swagger", swaggerFile = "openapi/documentation.yaml")
+
         get("/") {
             call.respondText("No customers found", status = HttpStatusCode.OK)
         }
